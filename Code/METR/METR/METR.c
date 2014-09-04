af5aa4338f583b16fb1f22234758868da5bb5133
Azuris <allenteh94@gmail.com>
2014-09-04 20:44:29 +1000

    Initial set up for ADC and entire Project

---
+++ Code/METR/METR/METR.c
@@ -0,0 +1,17 @@
+/*
+ * METR.c
+ *
+ * Created: 4/09/2014 8:30:46 PM
+ *  Author: User
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
