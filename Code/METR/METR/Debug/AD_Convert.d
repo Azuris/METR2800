95cac7841bbba75b7b247eeea10d3a0ada68bcab
Azuris <allenteh94@gmail.com>
2014-09-08 04:44:32 +1000

    Clock set up and mode choose.

--- Code/METR/METR/Debug/AD_Convert.d
+++ Code/METR/METR/Debug/AD_Convert.d
@@ -4,6 +4,7 @@ AD_Convert.d AD_Convert.o: .././AD_Convert.c \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/inttypes.h \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h \
+ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom328.h \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom328p.h \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/portpins.h \
  c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/common.h \
@@ -20,6 +21,8 @@ c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrt
 c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/include/stdint.h:
 
 c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/stdint.h:
+
+c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom328.h:
 
 c:\program\ files\atmel\atmel\ studio\ 6.0\extensions\atmel\avrgcc\3.4.1.95\avrtoolchain\bin\../lib/gcc/avr/4.6.2/../../../../avr/include/avr/iom328p.h:
 
--- Code/METR/METR/METR.c
+++ Code/METR/METR/METR.c
@@ -8,10 +8,31 @@
 
 #include <avr/io.h>
 
-int main(void)
-{
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+int main(void) {
+	uint8_t test;
+	
+	DDRD = 0x00;
+	while(1) {
+		if (PIND ^= 0x04) {
+			//run stationary single drop
+			DDRD |= (1 << DDD6);
+			TCCR0A |= 0b10000001;
+			TCCR0B |= 0b00001010;
+			//require timing for angles
+			// freq = (8MHz or 16Mhz)/8*510
+			return 0;
+		}
+		if (PIND ^= 0x05) {
+			//run moving single drop
+			return 0;
+		}
+		if (PIND ^= 0x06) {
+			//run ballistic run
+			return 0;
+		}
+		if (PIND ^= 0x07) {
+			//run moving multiple drop
+			return 0;
+		}
+	}	
 }
