eaa886f705395da5d7ba4a6e7caddf1abfdbef23
Azuris <allenteh94@gmail.com>
2014-09-10 17:36:45 +1000

    PWM Testing on Timer1

--- Code/METR/METR/METR.c
+++ Code/METR/METR/METR.c
@@ -5,6 +5,9 @@
  *  Author: User
  */ 
 
+#ifndef F_CPU
+#define F_CPU 2000000UL
+#endif
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -12,7 +15,20 @@
 int main(void) {
 	uint8_t test;
 	
+	ICR1 = 3125;
 	DDRD = 0x00;
+	DDRB |= (1 << DDB1);
+	while(1) {
+		PORTB ^= 0x02;
+		_delay_ms(200);
+	}
+	TCNT1 = 0;
+	TCCR1A |= (1 << COM1A1);
+	TCCR1B |= (1 << WGM13)|(1 << CS11)|(1 << CS10);
+				
+	OCR1A = 280;
+	
+	return 1;
 	while(1) {
 		if (PIND == 0x04) {
 			//run stationary single drop
